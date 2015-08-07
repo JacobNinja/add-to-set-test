4d0c9378cd50a75d83027b44980e4081e4bb0066
Jacob Richardson <jacob@rafflecopter.com>
2015-08-07 11:29:42 -0600

    Update script to insert smaller chunks of data. still erroring

--- src/add_to_set_test/core.clj
+++ src/add_to_set_test/core.clj
@@ -19,8 +19,16 @@
     (time (mc/update db coll
                      {:id "foo"}
                      {"$addToSet" {:data (into #{} (range 100000))}}))
-    (println "Adding 1,000,000 to set")
+    (println "Adding 500,000 to set")
     (time (mc/update db coll
                      {:id "foo"}
-                     {"$addToSet" {:data (into #{} (range 1000000))}}))
+                     {"$addToSet" {:data (into #{} (range 500000))}}))
+    (println "Adding additional [500,000 - 750,000] to set")
+    (time (mc/update db coll
+                     {:id "foo"}
+                     {"$addToSet" {:data (into #{} (range 500000 750000))}}))
+    (println "Adding additional [1,000,000 - 1,250,000] to set")
+    (time (mc/update db coll
+                     {:id "foo"}
+                     {"$addToSet" {:data (into #{} (range 1000000 1250000))}}))
     (md/drop-db db)))
